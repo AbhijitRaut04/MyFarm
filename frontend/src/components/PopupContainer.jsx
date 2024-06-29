8d71efc5e80880a4d64e2ec660bf12e551f23a39
Abhi Borkar <2022bcs096@sggs.ac.in>
2024-06-29 18:21:19 +0530

    view popup added

--- frontend/src/components/PopupContainer.jsx
+++ frontend/src/components/PopupContainer.jsx
@@ -1,69 +1,103 @@
-import React, { useContext, useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { SessionContext } from '../context/Contexts';
-import axios from 'axios';
-import ProfileCard from './ProfileCard';
-import { Link } from 'react-router-dom';
-import FontAwesomeIcon from 'react-fontawesome'
+import React, { useContext, useEffect, useState } from "react";
+import styled from "styled-components";
+import { SessionContext } from "../context/Contexts";
+import axios from "axios";
+import ProfileCard from "./ProfileCard";
+import { Link } from "react-router-dom";
+import FontAwesomeIcon from "react-fontawesome";
+import Loading from "./Loading";
+import { toast } from "react-toastify";
 
-const PopupContainer = ({ fetchRoute, setDisplay, Title }) => {
+const PopupContainer = ({ fetchRoute, setDisplay, type }) => {
+  const [farmers, setFarmers] = useState([]);
+  const { farmer } = useContext(SessionContext);
+  const [loading, setLoading] = useState(true);
 
-    const [farmers, setFarmers] = useState([]);
-    const { farmer } = useContext(SessionContext)
+  const showContainer = () => {
+    axios
+      .get(`${fetchRoute}`)
+      .then((response) => {
+        setFarmers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Error fetching farmers");
+      })
+      .finally(() => setLoading(false));
+  };
 
-    const showContainer = () => {
-        axios.get(`${fetchRoute}`)
-            .then((response) => {
-                setFarmers(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
-    const toggle = () => {
-        setDisplay(false);
-    }
+  const toggle = () => {
+    setDisplay(false);
+  };
 
-    useEffect(() => {
-        showContainer();
-    }, [farmer]);
+  useEffect(() => {
+    showContainer();
+  }, [farmer]);
 
-    return (
+  return (
+    <ContainerWrapper>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Container $count={farmers?.length || 0}>
+          <h3>
+            {type} by {farmers.length} Farmers
+          </h3>
+          <Cross onClick={toggle}>
+            <FontAwesomeIcon name="fa-solid fa-x" />
+          </Cross>
 
-        <Container $count={farmers}>
-            <h3>{Title}</h3>
-            <Cross onClick={toggle}>
-                <FontAwesomeIcon name="fa-solid fa-x" />
-            </Cross>
-            <ul>
-                {farmers.map((farmer) => (
-                    <Link key={farmer._id} to={`/profile/${farmer._id}`} style={{ textDecoration: "none" }} >
-                        <ProfileCard farmerId={farmer} />
-                    </Link>
-                ))}
-            </ul>
-
+          {farmers &&
+            farmers.map((farmer) => (
+              <div key={farmer._id}>
+                <ProfileCard showFarmer={farmer} />
+              </div>
+            ))}
         </Container>
-    )
-}
+      )}
+    </ContainerWrapper>
+  );
+};
 
-export default PopupContainer
+export default PopupContainer;
+
+const ContainerWrapper = styled.div`
+  width: 600px;
+  height: 100vh;
+  background-color: rgba(18, 8, 8, 0.1);
+  backdrop-filter: blur(2px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  margin: 0 auto;
+  @media (max-width: 600px) {
+    width: 100%;
+  }
+`;
 
 const Container = styled.div`
-    position:relative;
-    background-color: #e4e4e4;
-    padding-top:10px;
-    border-radius: 8px;
-    z-index:100;
-    height:${(props) => props.$count.length*55 + 50}px;
-    width: 100%;
-    h3{
-        text-align:center;
-        font-family:sans-serif;
-    }
-`
+  position: relative;
+  background-color: #fff;
+  height: ${(props) => props.$count * 55 + 50}px;
+  width: 400px;
+  border-radius: 1rem;
+  padding-top: 1rem;
+  margin: 10rem 2rem;
+  h3 {
+    font-weight: 600;
+    text-align: center;
+    color: #5a5a5a;
+  }
+`;
 const Cross = styled.div`
-    position:absolute;
-    top:10px;
-    right:10px;
-`
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  font-size: 0.8rem;
+  cursor: pointer;
+`;
--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -1,62 +1,102 @@
-import React, { useContext, useState } from 'react'
-import styled from 'styled-components'
-import { SessionContext } from '../context/Contexts'
+import React, { useContext, useEffect, useState } from "react";
+import styled from "styled-components";
+import { SessionContext } from "../context/Contexts";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const ProfileCard = ({farmerId}) => {
-    const {farmer} = useContext(SessionContext);
-    const [isFollowing, setFollowing] = useState(false);
-    if(farmer.following.includes(farmerId._id)){
-        setFollowing(true);
+const ProfileCard = ({ showFarmer }) => {
+  const { farmer } = useContext(SessionContext);
+  const [isFollower, setIsFollower] = useState(false);
+  useEffect(() => {
+    if (farmer.following.includes(showFarmer._id)) {
+      setIsFollower(true);
     }
-    return (
-        <Card>
-            <Image>
-                <img src={farmerId.profilePhoto} />
-            </Image>
-            <h4>{farmerId.username} </h4>
-            {/* {isFollowing ? 
-                <button>Unfollow</button>:
-                <button>Follow</button>
-            } */}
-        </Card>
-    )
-}
+    console.log(showFarmer);
+  }, [farmer]);
 
-export default ProfileCard
+  const navigate = useNavigate();
 
+  const toggleFollow = () => {
+    if (!isFollower) {
+      axios
+        .post(`/api/farmers/follow/${showFarmer._id}`)
+        .then((response) => {
+          console.log(response);
+          setIsFollower(true);
+        })
+        .catch((error) => {
+          console.error("Error: at featching profile", error);
+        });
+    } else {
+      axios
+        .post(`/api/farmers/unfollow/${showFarmer._id}`)
+        .then((response) => {
+          console.log(response);
+          setIsFollower(false);
+        })
+        .catch((error) => {
+          console.error("Error: at featching profile", error);
+        });
+    }
+  };
+
+  return (
+    <Card $isFollower={isFollower}>
+      <Image onClick={() => navigate(`/profile/${showFarmer._id}`)}>
+        <img src={showFarmer.profilePhoto} alt={showFarmer.username} />
+      </Image>
+      <h4 onClick={() => navigate(`/profile/${showFarmer._id}`)}>
+        {showFarmer.username}
+      </h4>
+      {showFarmer._id !== farmer._id && (
+        <button className="follow" onClick={toggleFollow}>
+          {isFollower ? "Unfollow" : "Follow"}
+        </button>
+      )}
+    </Card>
+  );
+};
+
+export default ProfileCard;
 
 const Card = styled.div`
-    padding:0 20px;
-    height:50px;
-    display:flex;
-    align-items:center;
+  /* background-color: #d58c8c; */
+  padding: 0 20px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  h4 {
+    color: #3a3a3a;
+    font-size: 1rem;
+    font-weight: 800;
+    cursor: pointer;
+  }
+  button {
+    margin-left: auto;
+    padding: 8px 10px;
+    background-color: #e7e7e7;
+    outline: none;
+    border: none;
+    color: #3a3a3a;
     border-radius: 8px;
-    gap:20px;
-    h4{
-        color:#000;
-        letter-spacing:1px;
-        font-family:sans-serif;
-    }
-    button{
-        margin-left:auto;
-        padding:8px 10px;
-        background-color:blue;
-        outline:none;
-        border:none;
-        color:white;
-        border-radius:8px;
-        font-size:15px;
-        font-weight:600;
-        letter-spacing:1px;
-        font-family:sans-serif;
-    }
-`
+    font-size: 14px;
+    font-weight: 600;
+    font-family: sans-serif;
+    cursor: pointer;
+  }
+  .follow {
+    background-color: ${(props) => (props.$isFollower ? "#e7e7e7" : "#ae2328")};
+    color: ${(props) => (props.$isFollower ? "#000000" : "#ffffff")};
+  }
+`;
 const Image = styled.div`
-    width:40px;
-    height:40px;
-    img{
-        width:100%;
-        border-radius:50%;
-        height:100%;
-    }
-`
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  img {
+    width: 100%;
+    border-radius: 50%;
+    height: 100%;
+  }
+`;
